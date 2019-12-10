6b74f938af71c79eb9c0d6d2e0c04ae3a2e39cdf
Levi Miller <paullevimiller@gmail.com>
2019-12-09 18:01:26 -0800

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,18 @@
 export function manageFriends(state, action){
-}
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      state.friends.push(action.friend);
+      return state;
+    case 'REMOVE_FRIEND':
+      let badFriendIndex = state.friends.findIndex(friend => friend.id === action.id)
+      let newState = {...state,
+            friends: [
+              ...state.friends.slice(0, badFriendIndex),
+              ...state.friends.slice(badFriendIndex + 1)
+            ]
+        };
+      return newState;
+    default:
+      return state;
+  };
+};
